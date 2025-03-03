2c8a14ec5276be0472ce5bd2ab201c7396b1c5b4
NNNNTTT <arsenal45@icloud.com>
2025-03-04 03:30:35 +0900

    フォルダをアップロードしました

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+<body>
+    <?php
+    $item = ["name" => "onion", "price" => 200, "weight" => 160];
+    print_r($item);
+    ?>
+</body>
+</html>
